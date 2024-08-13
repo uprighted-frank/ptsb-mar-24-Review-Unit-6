079c4de60964ab9f7df7a7c09cfbfa7ca9cfe05e
Frank Chambergo <franky4908@gmail.com>
2024-08-12 20:43:44 -0400

    mongo atlast uri

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/shop");
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error!"));
